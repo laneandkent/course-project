65c329cf21a2a8303e24f813200454c553177e43
Missy <82602150+laneandkent@users.noreply.github.com>
2021-05-26 22:23:31 -0500

    Minor changes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,3 @@
-import { EventEmitter } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from './recipe.model';
 import { Injectable } from '@angular/core'
@@ -12,7 +11,6 @@ export class RecipeService {
     new Recipe('Cheeseburger', 'The Perfect Sandwich', 'https://images.unsplash.com/photo-1551987840-f62d9c74ae78?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1488&q=80', [ new Ingredient('Ground Beef Patty', 1), new Ingredient('Tomato', 1)]),
     new Recipe('Pizza', 'Not Just Your Basic Pie', 'https://images.unsplash.com/photo-1590947132387-155cc02f3212?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80', [new Ingredient('Pizza Crust', 1), new Ingredient('Basil', 3)])
   ];
-  recipeSelected = new EventEmitter<Recipe>();
 
   constructor(private slService: ShoppingListService){}
 
